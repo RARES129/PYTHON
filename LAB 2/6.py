63114a7236ff55748a249381ae1b8b87b85bcdad
RARES129 <dascaluraresm@gmail.com>
2023-10-25 06:46:24 +0300

    Update 6.py

--- LAB 2/6.py
+++ LAB 2/6.py
@@ -1,20 +1,21 @@
 def counter(*lists, x):
-    # new_list = [i for list in lists for i in list]
-    new_list  = list(set(new_list))
+    new_list = set([i for list in lists for i in list])
 
     result = []
     count = 0
-    for i in new_list:
-        for l in lists:
-            if i in l:
-                count += l.count(i)
+    for index1 in new_list:
+        for index2 in lists:
+            if index1 in index2:
+                count += index2.count(index1)
         if count == x:
-            result.append(i)
+            result.append(index1)
         count = 0
-    
+
     return result
 
+
 def main():
-    print(counter([1,2,3], [2,3,4],[4,5,6], [4,1, "test"], x = 2))
+    print(counter([1, 2, 3], [2, 3, 4], [4, 5, 6], [4, 1, "test"], x=2))
+
 
 main()
