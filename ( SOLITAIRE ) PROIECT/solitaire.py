4704ebaf1a72fb676295f4a4c98af40b6b0549a7
Dascălu Rareș <dascaluraresm@gmail.com>
2024-01-16 06:36:33 +0200

    Adaugare clasa unei carti de joc

--- ( SOLITAIRE ) PROIECT/solitaire.py
+++ ( SOLITAIRE ) PROIECT/solitaire.py
@@ -8,8 +8,8 @@ pygame.display.set_caption("Solitaire")
 window = pygame.display.set_mode((width, height))
 background_color = (0, 128, 0)  # RGB value for dark green
 
-pygame.display.flip()
 
+pygame.display.flip()
 
 """ Functie pentru a incarca o imagine si a o scala la dimensiunile dorite """
 
@@ -18,3 +18,66 @@ def UploadImage(image_path, width, height):
     image = pygame.image.load(image_path)
     scaled_image = pygame.transform.scale(image, (width, height))
     return scaled_image
+
+
+# ---------------------------CARTI DE JOC---------------------------
+""" Clasa pentru cartile de joc."""
+
+
+class Card:
+    """Constructorul clasei"""
+
+    def __init__(self, color, suit, number):
+        self.color = color
+        self.suit = suit
+        self.number = number
+        self.faceup = False
+        self.image = UploadImage(
+            "PNG-cards/" + str(number) + "_of_" + suit + ".png", 66, 100
+        )
+        self.cardBack = UploadImage("PNG-cards/cardback.png", 66, 100)
+        self.imageShow = self.cardBack
+        self.border_color = (255, 0, 0)
+        self.has_border = False
+
+    """Metoda pentru a desena cartea de joc"""
+
+    def draw(self, x, y):
+        window.blit(self.imageShow, (x, y))
+
+    """Metoda pentru a obtine culoarea cartii"""
+
+    def get_colour(self):
+        return self.color
+
+    """Metoda pentru a intoarce cartea de joc"""
+
+    def flip(self):
+        self.faceup = not self.faceup
+        if self.faceup:
+            self.imageShow = self.image
+        else:
+            self.imageShow = self.cardBack
+
+    """Metoda pentru a adauga sau elimina bordura cartii de joc"""
+
+    def set_border(self):
+        self.has_border = not self.has_border
+
+    """Metoda pentru a verifica daca cartea este de culoare opusa cu cartea data ca parametru"""
+
+    def isOppositeColour(self, other_card):
+        if self.color == "red" and other_card.color == "black":
+            return True
+        elif self.color == "black" and other_card.color == "red":
+            return True
+        else:
+            return False
+
+    """Metoda pentru a verifica daca cartea este cu un numar mai mica decat cartea data ca parametru"""
+
+    def IsOneLessThan(self, other_card):
+        if self.number == other_card.number - 1:
+            return True
+        else:
+            return False
