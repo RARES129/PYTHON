bffca77dfdd94eb8e68c249613b972cb5db24bc1
Dascălu Rareș <dascaluraresm@gmail.com>
2024-01-16 06:35:40 +0200

    Functie de upload si redimensionare pentru imagini

--- ( SOLITAIRE ) PROIECT/solitaire.py
+++ ( SOLITAIRE ) PROIECT/solitaire.py
@@ -8,5 +8,13 @@ pygame.display.set_caption("Solitaire")
 window = pygame.display.set_mode((width, height))
 background_color = (0, 128, 0)  # RGB value for dark green
 
-
 pygame.display.flip()
+
+
+""" Functie pentru a incarca o imagine si a o scala la dimensiunile dorite """
+
+
+def UploadImage(image_path, width, height):
+    image = pygame.image.load(image_path)
+    scaled_image = pygame.transform.scale(image, (width, height))
+    return scaled_image
